67dfe8d5a0ac6561a94954cb3d34ad230cd35244
a-kobori <89172056+a-kobori@users.noreply.github.com>
2023-08-06 16:09:09 +0900

    バックアップファイルが作成されないようにする

--- install.sh
+++ install.sh
@@ -11,14 +11,18 @@ PROJECT_NAME_WITHOUT_HYPHEN=${PROJECT_NAME//-/_}
 # リネーム処理
 mv koboripackage $PROJECT_NAME_WITHOUT_HYPHEN
 sed -i -e "s/koboripackage/$PROJECT_NAME_WITHOUT_HYPHEN/g" ./tests/test_main.py
+rm -f ./tests/test_main.py-e
 
 # Dockerfile
 sed -i -e "s/koboripackage/$PROJECT_NAME_WITHOUT_HYPHEN/g" Dockerfile
+rm -f Dockerfile-e
 
 # setup.py
 sed -i -e "s/name=\"koboripackage\"/name=\"$PROJECT_NAME\"/g" setup.py
 sed -i -e "s/python_package/$PROJECT_NAME_WITHOUT_HYPHEN/g" setup.py
 sed -i -e "s/koboripackage = koboripackage.cli:execute/$PROJECT_NAME = $PROJECT_NAME_WITHOUT_HYPHEN.cli:execute/g" setup.py
+rm -f setup.py-e
 
 # .gitignore
 sed -i -e "s/koboripackage/$PROJECT_NAME/g" .gitignore
+rm -f .gitignore-e
